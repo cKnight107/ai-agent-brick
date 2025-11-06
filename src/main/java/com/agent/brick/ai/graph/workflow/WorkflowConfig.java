5b78fda37a4cfde05489eab6661afc9f1f597a21
cKnight <chenkai107@foxmail.com>
2025-11-06 13:56:40 +0800

    feat:config

---
+++ src/main/java/com/agent/brick/ai/graph/workflow/WorkflowConfig.java
@@ -0,0 +1,17 @@
+package com.agent.brick.ai.graph.workflow;
+
+import com.alibaba.cloud.ai.graph.StateGraph;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * 工作流配置
+ * </p>
+ *
+ * @author cKnight
+ * @since 2025/11/6
+ */
+@Configuration
+public class WorkflowConfig {
+}
